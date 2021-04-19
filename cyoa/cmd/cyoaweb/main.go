dc74ad9e1ab3f4f170e49436fdc34c05b0fe1120
ZiyanK <ziyankarmali786@gmail.com>
2021-04-19 23:59:32 +0530

    Added options to the package

--- cyoa/cmd/cyoaweb/main.go
+++ cyoa/cmd/cyoaweb/main.go
@@ -4,9 +4,11 @@ import (
 	"cyoa"
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -25,7 +27,45 @@ func main() {
 		panic(err)
 	}
 
-	h := cyoa.NewHandler(story)
+	tpl := template.Must(template.New("").Parse(storyTmpl))
+	h := cyoa.NewHandler(story,
+		cyoa.WithTemplate(tpl),
+		cyoa.WithPathFunc(pathFn),
+	)
+	mux := http.NewServeMux()
+	mux.Handle("/story/", h)
 	fmt.Printf("Starting the server at :%d\n", *port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
+
+func pathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "/story" || path == "/story/" {
+		path = "/story/intro"
+	}
+
+	return path[len("/story/"):]
+}
+
+var storyTmpl = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Choose your own adventure</title>
+</head>
+<body>
+	<h1>{{.Title}}</h1>
+	{{range .Paragraphs}}
+		<p>{{.}}</p>
+	{{end}}
+	<ul>
+		{{range .Options}}
+			<li><a href="/story/{{.Chapter}}">{{.Text}}</a></li>
+		{{end}}
+	</ul>
+</body>
+</html>
+`
--- cyoa/story.go
+++ cyoa/story.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"html/template"
 	"io"
+	"log"
 	"net/http"
+	"strings"
 )
 
 func init() {
@@ -36,19 +38,55 @@ var defaultHandlerTmpt = `
 </html>
 `
 
-func NewHandler(s Story) handler {
-	return handler{s}
+type HandlerOption func(h *handler)
+
+func WithTemplate(t *template.Template) HandlerOption {
+	return func(h *handler) {
+		h.t = t
+	}
+}
+
+func WithPathFunc(fn func(r *http.Request) string) HandlerOption {
+	return func(h *handler) {
+		h.pathFn = fn
+	}
+}
+
+func NewHandler(s Story, opts ...HandlerOption) http.Handler {
+	h := handler{s, tpl, defaultPathFn}
+	for _, opt := range opts {
+		opt(&h)
+	}
+	return h
 }
 
 type handler struct {
-	s Story
+	s      Story
+	t      *template.Template
+	pathFn func(r *http.Request) string
+}
+
+func defaultPathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "" || path == "/" {
+		path = "/intro"
+	}
+
+	return path[1:]
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	err := tpl.Execute(w, h.s["intro"])
-	if err != nil {
-		panic(err)
+	path := h.pathFn(r)
+
+	if chapter, ok := h.s[path]; ok {
+		err := h.t.Execute(w, chapter)
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		}
+		return
 	}
+	http.Error(w, "Chapter not found", http.StatusNotFound)
 }
 
 func JsonStory(r io.Reader) (Story, error) {
