4f3301fbe14a156e21856fd11498c081115bbfe3
ZiyanK <ziyankarmali786@gmail.com>
2021-04-21 20:15:34 +0530

    added functionality to cobra commands

--- task/cmd/do.go
+++ task/cmd/do.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"strconv"
+	"task/db"
 
 	"github.com/spf13/cobra"
 )
@@ -19,6 +20,24 @@ var doCmd = &cobra.Command{
 				continue
 			}
 			ids = append(ids, id)
+		}
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Something went wrong: ", err)
+			return
+		}
+		for _, id := range ids {
+			if id <= 0 || id > len(tasks) {
+				fmt.Println("Invalid task number: ", id)
+				continue
+			}
+			task := tasks[id-1]
+			err := db.DeleteTask(task.Key)
+			if err != nil {
+				fmt.Printf("Failed to mark \"%d\" as complete. Error :%s\n", id, err)
+			} else {
+				fmt.Printf("Marked \"%d\" as completed.", id)
+			}
 		}
 		fmt.Println(ids)
 	},
