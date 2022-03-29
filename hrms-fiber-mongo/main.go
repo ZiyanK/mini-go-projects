71c3ba94a7660132ac7b789c16a760f3bba4c6c5
ZiyanK <ziyankarmali786@gmail.com>
2022-03-29 11:42:04 +0530

    added hrms fiber mongo

---
+++ hrms-fiber-mongo/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoInstance struct {
+	Client *mongo.Client
+	Db     *mongo.Database
+}
+
+var mg MongoInstance
+
+const dbName = "hrms"
+const mongoURI = "mongodb://localhost:27017" + dbName
+
+type Employee struct {
+	ID     int64   `json:"id,omitempty" bson:"_id, omitempty"`
+	Name   string  `json:"name"`
+	Salary float64 `json:"salary"`
+	Age    int32   `json:"age"`
+}
+
+func Connect() error {
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	db := client.Database(dbName)
+
+	if err != nil {
+		return err
+	}
+
+	mg = MongoInstance{
+		Client: client,
+		Db:     db,
+	}
+	return nil
+}
+
+func main() {
+	if err := Connect(); err != nil {
+		log.Fatalf("Error connecting to db: %v", err)
+	}
+	app := fiber.New()
+	app.Get("/employee", func(c *fiber.Ctx) error {
+		query := bson.D{{}}
+
+		curosr, err := mg.Db.Collection("employees").Find(c.Context(), query)
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+		var employees []Employee = make([]Employee, 0)
+
+		if err := curosr.All(c.Context(), &employees); err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+
+		return c.Status(200).JSON(employees)
+	})
+	app.Post("/employee")
+	app.Put("/employee/:id")
+	app.Delete("/employee/:id")
+
+}
